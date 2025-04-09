e5d7edd54c835b600b44f3b86a5cccc8f98f9ed9
trym12 <trymhmerk@gmail.com>
2025-04-09 11:43:32 +0200

    start
    setter opp oppgaven

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="app">
+	
+
+	<main>
+		{@render children()}
+	</main>
+
+</div>
+
+<style>
+	
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	let array = [1,2,3,4,5];
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
